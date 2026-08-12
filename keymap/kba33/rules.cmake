6f8d5a665ba13b510b38331d5dbdb2b09ccbecbf
kbladewht <w00110601111@163.com>
2026-08-12 20:05:22 +0800

    add

--- keymap/kba33/rules.cmake
+++ keymap/kba33/rules.cmake
@@ -8,7 +8,6 @@ set(RGB_MATRIX_DRIVER "ws2812" CACHE STRING "KB" FORCE)
 set(WS2812_DRIVER "pwm" CACHE STRING "KB" FORCE)
 set(BLE_ENABLE ON CACHE BOOL "KB" FORCE)
 set(UF2_FAMILY_ID "0x621e937a" CACHE STRING "KB" FORCE)
-set(CUSTOM_MATRIX "lite" CACHE STRING "KB" FORCE)
 
 # mxkbd_append_user_src(esb_send.c)
 mxkbd_append_user_src(../../../cross/esb_send.c)
@@ -23,12 +22,6 @@ mxkbd_append_user_src(../../../cross/keys_hold.c)
 mxkbd_append_user_src(../../../cross/ble_major.c)
 mxkbd_append_user_src(../../../cross/qf_channel_manage.c)
 mxkbd_append_user_src(../../../cross/rtc_qf.c)
-mxkbd_append_user_src(../../../common/74hc595.c)
-# mxkbd_append_user_src(matrix_qf.c) inlcuded in parrent folder matrix_scan_user
-
-mxkbd_append_user_src(../../../common/matrix_l3.c)
-
-mxkbd_append_user_src(../../../common/rgb_t.c)
 
 mxkbd_set_user_ld(Ld/nrf52833.ld)
 add_compile_definitions(QMK_MCU_NRF52833)
